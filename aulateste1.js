45d54df4db2cb6b528291db5ec1246c07c731f52
Edilson092 <139914097+Edilson092@users.noreply.github.com>
2023-07-25 20:00:05 +0000

    corrigindo o assunto da aula agora sim javascript.

---
+++ aulateste1.js
@@ -0,0 +1,92 @@
+// console.log('qual sua idade?')
+// var nome='edilson';
+// console.log(nome);
+// var idade = 30;
+// console.log(idade);
+// if(idade>=18){
+//     console.log('voce pode dirigir '+ nome);
+// } else if(idade>=16){
+//     console.log('você pode dirigir com permissão dos pais! '+ nome);
+// } else {
+//     console.log('você não pode dirigir! '+ nome); 
+// }
+
+// var diaDaSemana = 3;
+// switch(diaDaSemana){
+//     case 1:
+//         console.log('hoje é segunda-feira!');
+//         break;
+//     case 2:
+// console.log('hoje é terça-feira!')
+//     case 3:
+
+// console.log('hoje é quarta-feira')
+//     case 4:
+//         break;
+// console.log('hoje é quinta-feira')
+//     case 5:
+// console.log('hoje é sexta-feira!')
+//         break
+//     case 6:
+// console.log('hoje é sabado!')
+//         break
+//     case 7:
+// console.log('hoje é domingo!')
+//         break
+// }
+
+// var numeroDoMes = 1
+// switch (numeroDoMes) {
+//     case 1:
+//         console.log('estamos em janeiro!')
+//         break
+//     case 2:
+//         console.log('estamos em fevereriro!')
+//         break
+//     case 3:
+//         console.log('estamos em março')
+//         break
+//     case 4:
+//         console.log('estamos em abril!')
+//         break
+//     case 5:
+//         console.log('estamos em abril!')
+//         break
+//     case 6:
+//         console.log('estamnos em junho!')
+//         break
+//     case 7:
+//         console.log('estamos em julho!')
+//         break
+//     case 8:
+//         console.log('estamos em agosto!')
+//         break
+//     case 9:
+//         console.log('estamos em setembro!')
+//     case 10:
+//         console.log('estamos em outubro!')
+//         break
+//     case 11:
+//         console.log('estamos em novembro!')
+//         break
+//     case 12:
+//         console.log('estamos em dezembro!')
+//         break
+// }
+
+
+//var estadoCivil = 4
+//switch (estadoCivil) {
+//    case 1:
+//        console.log('você é solteiro!')
+//       break
+//    case 2:
+//       console.log('vc é casado!')
+//        break
+//    case 3:
+//        console.log('você é viuvo!')
+//        break
+//    default: 
+//        console.log('vc está enrolado!')
+//      break
+//}
